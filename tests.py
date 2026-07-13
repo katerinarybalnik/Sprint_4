c7e981c4b3f2bc58d8cfe0eb7897bb97d63d1c50
Katerina Rybalnik <ekaterinarybalnik@yandex.com>
2026-07-13 03:05:05 +0300

    Add qa tests

--- tests.py
+++ tests.py
@@ -1,4 +1,5 @@
 from main import BooksCollector
+import pytest
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
 # обязательно указывать префикс Test
@@ -17,8 +18,107 @@ class TestBooksCollector:
         collector.add_new_book('Что делать, если ваш кот хочет вас убить')
 
         # проверяем, что добавилось именно две
-        # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        # словарь books_genre, который нам возвращает метод get_books_genre, имеет длину 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниже
-    # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+        #проверка,что у новой книги нет жанра
+    def test_add_new_book_has_empty_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Фиксики')
+
+        assert collector.get_book_genre('Фиксики') == ''
+    
+        # Проверка, что устанавливается жанр книги
+    def test_set_book_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Феиринки')
+        collector.set_book_genre('Феиринки', 'Мультфильмы')
+
+        assert collector.get_book_genre('Феиринки') == 'Мультфильмы'
+    
+    
+        # Проверка получения жанра книги по названию
+    def test_get_book_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Феиринки')
+        collector.set_book_genre('Феиринки', 'Мультфильмы')
+
+        assert collector.get_book_genre('Феиринки') == 'Мультфильмы'
+        
+    
+        # Проверка получения списка книг определенного жанра
+    def test_get_books_with_specific_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Феиринки')
+        collector.set_book_genre('Феиринки', 'Мультфильмы')
+
+        collector.add_new_book('Один дома')
+        collector.set_book_genre('Один дома', 'Ужасы')
+
+        assert collector.get_books_with_specific_genre('Мультфильмы') == ['Феиринки']
+    
+        # Проверка получения словаря всех книг
+    def test_get_books_genre(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Феиринки')
+
+        assert collector.get_books_genre() == {'Феиринки': ''}
+
+        # Проверка книг, которые подходят детям
+    @pytest.mark.parametrize('genre, result', [('Мультфильмы',True),('Ужасы', False),('Детективы', False)])
+    def test_get_books_for_children(self, genre, result):
+        collector = BooksCollector()
+
+        collector.add_new_book('Книга')
+
+        collector.set_book_genre('Книга', genre)
+
+        books = collector.get_books_for_children()
+
+        if result:
+            assert 'Книга' in books
+        else:
+            assert 'Книга' not in books
+
+        # Проверка добавления книги в избранное
+    def test_add_book_in_favorites(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Поющие в терновнике')
+        collector.add_book_in_favorites('Поющие в терновнике')
+
+        assert collector.get_list_of_favorites_books() == ['Поющие в терновнике']
+
+
+        # Проверка удаления книги из избранного
+    def test_delete_book_from_favorites(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Поющие в терновнике')
+        collector.add_book_in_favorites('Поющие в терновнике')
+
+        collector.delete_book_from_favorites('Поющие в терновнике')
+
+        assert collector.get_list_of_favorites_books() == []
+
+        # Проверка получения списка избранных книг
+    def test_get_list_of_favorites_books(self):
+        collector = BooksCollector()
+
+        collector.add_new_book('Поющие в терновнике')
+        collector.add_book_in_favorites('Поющие в терновнике')
+
+        assert collector.get_list_of_favorites_books() == ['Поющие в терновнике']
+
+
+
+    
+
+
+
+
